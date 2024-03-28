b8a05f5770bae6da24eb755653372dd13cddb0ab
MatplotlibCircleBot <MatplotlibCircleBot@nomail>
2024-03-28 07:07:10 +0000

    Docs build of 0afc5d6ca49cf6e8aa1da76b5bab0faca2f340f2

---
+++ _downloads/553f9fcc4fa403ef21dded13a773ae47/update_arrow_patch-1.py
@@ -0,0 +1,21 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.patches import Arrow
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+
+a = mpl.patches.Arrow(2, 0, 0, 10)
+ax.add_patch(a)
+
+
+# code for modifying the arrow
+def update(i):
+    a.set_data(x=.5, dx=i, dy=6, width=2)
+
+
+ani = animation.FuncAnimation(fig, update, frames=15, interval=90, blit=False)
+
+plt.show()
